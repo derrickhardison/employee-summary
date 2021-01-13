7cdb18f387909d6c37b4114739a2d684d996a4d3
dlhardi <derricklhardison@gmail.com>
2021-01-13 10:02:00 -0500

    removed unnecessary files. added modeule export to Employee.js and tested it. It passed the test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require("../lib/Manager");
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -8,7 +8,7 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+const render = require("../lib/htmlRenderer");
 
 
 // Write code to use inquirer to gather information about the development team members,
@@ -33,3 +33,8 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+// who is the manager?
+// inside .then, call another function that asks, do you want to add someone else t your team
+// based on that response you call the engineer response and so forth, are you finished adding to the team?
+// 
