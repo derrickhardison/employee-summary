5812ff988ef4461a181522206312f5d9e61a471a
dlhardi <derricklhardison@gmail.com>
2021-01-14 16:27:35 -0500

    added functions to prompt questions. created output folder

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const Manager = require("../lib/Manager");
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -8,11 +8,158 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("../lib/htmlRenderer");
+const render = require("./lib/htmlRenderer");
 
+const employees = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function createManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the manager's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the manager's id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+      },
+    ])
+    .then((response) => {
+      const manager = new Manager(
+        response.name,
+        response.id,
+        response.email,
+        response.officeNumber
+      );
+      employees.push(manager);
+
+      nextEmployee();
+    });
+}
+
+function nextEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Who else would you like to add to the team?",
+        name: "name",
+        choices: ["Intern", "Engineer", "Done"],
+      },
+    ])
+    .then((response) => {
+      if (response.name === "Intern") {
+        createIntern();
+      } else if (response.name === "Engineer") {
+        createEngineer();
+      } else {
+        createTeam();
+      }
+    });
+}
+
+function createIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the intern's id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the intern's school?",
+      },
+    ])
+    .then((response) => {
+      const intern = new Intern(
+        response.name,
+        response.id,
+        response.email,
+        response.school
+      );
+      employees.push(intern);
+
+      nextEmployee();
+    });
+}
+
+function createEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the engineer's Github?",
+      },
+    ])
+    .then((response) => {
+      const engineer = new Engineer(
+        response.name,
+        response.id,
+        response.email,
+        response.github
+      );
+      employees.push(engineer);
+
+      nextEmployee();
+    });
+}
+
+function createTeam(){
+  fs.writeFileSync(outputPath, render(employees), "utf8");
+}
+
+//FUNCTION CALLS
+
+
+
+createManager();
+
+// need createTeam() that will use hTML render to render HTML - pass employees array
+// insert below inside createTeam()
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -37,4 +184,4 @@ const render = require("../lib/htmlRenderer");
 // who is the manager?
 // inside .then, call another function that asks, do you want to add someone else t your team
 // based on that response you call the engineer response and so forth, are you finished adding to the team?
-// 
+//
